b184058b5bc08446000959d183075b4e208fc5fc
myakubov18 <michael.yakubov22@gmail.com>
2022-11-14 21:02:13 -0500

    added DOB to registration, made dollar amounts allow cents

--- server.py
+++ server.py
@@ -798,9 +798,10 @@ def register():
     else:
         password = info['password']
         name = info['name']
+        dob = info['dob']
         user_row = g.conn.execute(
-            text("INSERT INTO Users(name, email, password) VALUES(:name,:email,:pwd) RETURNING id"),
-            {'name': name, 'email': email, 'pwd': password}).fetchone()
+            text("INSERT INTO Users(name, email, password, date_of_birth) VALUES(:name,:email,:pwd, :dob) RETURNING id"),
+            {'name': name, 'email': email, 'pwd': password, 'dob': dob}).fetchone()
         result = g.conn.execute("SELECT * FROM Users where id=%s", user_row.id)
         user = User.from_row(result.fetchone())
         login_user(user)
